bb0c28126085788f226498b9c82abf1435148646
nilayupadhyay17 <nilay175@gmail.com>
2025-04-02 13:02:17 -0400

    added nginx config

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const BASE_URL_Task = process.env.NEXT_PUBLIC_API_URL_TASK || "http://taskmanager-lb-38865464.us-east-1.elb.amazonaws.com"; 
-const BASE_URL_AUTH = process.env.NEXT_PUBLIC_API_URL_AUTH || "http://taskmanager-lb-38865464.us-east-1.elb.amazonaws.com";
+const BASE_URL_Task = process.env.NEXT_PUBLIC_API_URL_TASK || "http://internal-taskmanager-internal-lb-1644733959.us-east-1.elb.amazonaws.com"; 
+const BASE_URL_AUTH = process.env.NEXT_PUBLIC_API_URL_AUTH || "http://internal-taskmanager-internal-lb-1644733959.us-east-1.elb.amazonaws.com";
 
 // Utility function to get the token from localStorage
 const getToken = () => {
